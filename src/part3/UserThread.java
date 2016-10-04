c842e3258a76b341e4056d2f31785c9ace9796cb
Lei Yang <lei.yang.cs.art@gmail.com>
2016-10-04 18:25:19 -0400

    Add unicast function, user can send private message through @xx message

--- src/part3/UserThread.java
+++ src/part3/UserThread.java
@@ -102,20 +102,82 @@ class UserThread extends Thread {
 
 	}
 
+	public boolean isUnicast(String s) {
+		String[] message = s.split("\\s+");
+		if (message[0].charAt(0) == '@') {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public String getUnicastUserName(String[] message) {
+		return message[0].substring(1, message[0].length());
+	}
+
+	public String getUnicastMessage(String s, String[] message) {
+		if (s.length() < message[0].length() + 1) {
+			return null;
+		}
+		return s.substring(message[0].length() + 1);
+	}
+
 	public void unicast(String s) {
-		String message = s.split("\\s+");
+		String[] messages = s.split("\\s+");
+		String unicastUser = getUnicastUserName(messages);
+		if (unicastUser == null || !isUserExist(unicastUser)) {
+			return;
+		}
+		String message = getUnicastMessage(s, messages);
+		if (!isValidMessage(message)) {
+			return;
+		}
 		try {
+			output_stream = new PrintStream(userSocket.getOutputStream(), true);
+			output_stream.println("<" + userName + ">: " + message);
 			for (UserThread thread : threads) {
-				// The next thread need wait until the previous finished
-				synchronized (thread) {
-					thread.output_stream = new PrintStream(thread.userSocket.getOutputStream(), true);
-					thread.output_stream.println("<" + userName + ">: " + s);
+				if (thread.userName.equals(unicastUser)) {
+					// synchronize the thread to prevent other visit
+					synchronized (thread) {
+						thread.output_stream = new PrintStream(thread.userSocket.getOutputStream(), true);
+						thread.output_stream.println("<" + userName + ">: " + message);
+					}
 				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+	}
+
+	public boolean isUserExist(String s) {
+		for (UserThread thread : threads) {
+			if (thread.userName != null && thread.userName.equals(s)) {
+				return true;
+			}
+		}
+		try {
+			output_stream = new PrintStream(userSocket.getOutputStream(), true);
+			output_stream.println(s + " not exists, please the unicast name");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+
+	public boolean isValidMessage(String s) {
+		try {
+			if (s == null || s == "") {
+				output_stream = new PrintStream(userSocket.getOutputStream(), true);
+				output_stream.println("Please input something");
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return true;
+
 	}
 
 	public void broadcast(String s) {
@@ -143,6 +205,8 @@ class UserThread extends Thread {
 					if (s.toLowerCase().equals("logout")) {
 						logout();
 						return;
+					} else if (isUnicast(s)) {
+						unicast(s);
 					} else {
 						broadcast(s);
 					}
